2aec32d893f1f12d8153542309f7f54a9b10efc4
Nik <nikitanikolaev01081992@gmail.com>
2021-01-31 05:18:26 +0300

    Задание 2. Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const getRandomFloat = (min, max, precision = 2) => {
+  if (min > max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+
+  let randNum = Math.random() * (max - min + 0.001) + min;
+  randNum = randNum > max ? max : randNum;
+
+  return parseFloat(randNum.toFixed(precision));
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return getRandomFloat(min, max, 0);
+};
