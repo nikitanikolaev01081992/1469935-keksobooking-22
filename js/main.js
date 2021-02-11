b4a666dc2c070c7e2b15a8e5d9ecfd972da13cd5
Nik <nikitanikolaev01081992@gmail.com>
2021-02-11 23:57:45 +0300

    Правки для задания 3

--- js/main.js
+++ js/main.js
@@ -1,30 +1,49 @@
+const PRICE_MAX = 1000000;
+const ROOMS_MAX = 100;
+const TYPE = ['palace', 'flat', 'house', 'bungalow'];
+const TIME = ['14:00', '13:00', '12:00']; //this array should be in descending order
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const CoordinateRanges = {
+  latitudeMin: 35.65,
+  latitudeMax: 35.7,
+  longitudeMin: 139.7,
+  longitudeMax: 139.8,
+};
+
+const MinPricesByType = {
+  palace: 10000,
+  house: 5000,
+  flat: 1000,
+  bungalow: 0,
+};
+
 const getRandomFloat = (min, max, precision = 2) => {
-  if (
-    isNaN(min) ||
-    isNaN(min) ||
-    isNaN(precision) ||
-    min < 0 ||
-    max < 0 ||
-    precision < 0
-  ) {
+  if (isNaN(min) || isNaN(min) || isNaN(precision) || min < 0 || max < 0 || precision < 0) {
     throw new Error('getRandomFloat: Неверные входные параметры');
   }
 
-  if (min > max) {
-    [min, max] = [max, min];
+  let [localMin, localMax] = min > max ? [max, min] : [min, max];
+
+  let randNum = Math.random() * (localMax - localMin) + localMin;
+
+  if (Math.random() > 0.5) {
+    randNum += 1 / Math.pow(10, precision);
   }
+  randNum = Math.trunc(randNum * Math.pow(10, precision)) / Math.pow(10, precision);
 
-  let randNum = Math.random() * (max - min) + min;
-  randNum = randNum > max ? max : randNum;
-
-  return parseFloat(randNum.toFixed(precision));
+  return randNum;
 };
 
 const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  let [localMin, localMax] = [Math.ceil(min), Math.floor(max)];
 
-  return getRandomFloat(min, max, 0);
+  return getRandomFloat(localMin, localMax, 0);
 };
 
 //function returns random value from array
@@ -32,7 +51,9 @@ const getRandElemFromArray = (elements) => {
   if (!Array.isArray(elements)) {
     throw new Error('getRandElemFromArr: Неверные входные параметры');
   }
-  if (elements.length === 0) return undefined;
+  if (elements.length === 0) {
+    return undefined;
+  }
 
   return elements[getRandomInt(0, elements.length - 1)];
 };
@@ -43,52 +64,16 @@ const getRandArrayFromValues = (values) => {
     throw new Error('getRandArrayFromValues: Неверные входные параметры');
   }
 
-  let availableValues = values.slice();
-  const lengthOfArray = getRandomInt(0, values.length);
-  let dummyValues = new Array(lengthOfArray).fill(null);
-
-  return dummyValues.map(() => {
-    const newItem = getRandElemFromArray(availableValues);
-    availableValues.splice(availableValues.indexOf(newItem), 1);
-
-    return newItem;
+  return values.filter(() => {
+    return Math.random() > 0.5;
   });
 };
 
 //DATA GENERATOR
-const LATITUDE_MIN = 35.65;
-const LATITUDE_MAX = 35.7;
-const LONGITUDE_MIN = 139.7;
-const LONGITUDE_MAX = 139.8;
-const PRICE_MAX = 1000000;
-const ROOMS_MAX = 100;
-const TYPE = ['palace', 'flat', 'house', 'bungalow'];
-const TIME = ['14:00', '13:00', '12:00']; //this array should be in descending order
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
-
-const MinPricesByType = {
-  palace: 10000,
-  house: 5000,
-  flat: 1000,
-  bungalow: 0,
-};
-
 const getRandDataObject = (id = 1) => {
   const location = {
-    x: getRandomFloat(LATITUDE_MIN, LATITUDE_MAX, 5),
-    y: getRandomFloat(LONGITUDE_MIN, LONGITUDE_MAX, 5),
+    x: getRandomFloat(CoordinateRanges.latitudeMin, CoordinateRanges.latitudeMax, 5),
+    y: getRandomFloat(CoordinateRanges.longitudeMin, CoordinateRanges.longitudeMax, 5),
   };
 
   const type = getRandElemFromArray(TYPE);
@@ -122,7 +107,17 @@ const getRandDataObject = (id = 1) => {
   };
 };
 
-let dummyData = new Array(10).fill(null).map((elem, index) => {
-  return getRandDataObject(index + 1);
-});
-dummyData;
+const generateDummyData = (counts = 10) => {
+  if (isNaN(counts) || counts < 0) {
+    throw new Error('generateDummyData: Неверные входные параметры');
+  }
+
+  let dummyData = [];
+  for (let i = 0; i < counts; i++) {
+    dummyData.push(getRandDataObject(i + 1));
+  }
+
+  return dummyData;
+};
+
+generateDummyData();
