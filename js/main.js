9132d72a36132ade6a05e24e4fc9b68ec8a37eb3
Nik <nikitanikolaev01081992@gmail.com>
2021-02-22 03:45:45 +0300

    Добавил исключение если не найден контейнер для офферов

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ import { getOfferNode } from './offers.js';
 // Constants
 const OFFERS_CONTAINER = document.querySelector('.map__canvas');
 
+if (!OFFERS_CONTAINER) {
+  throw new Error('OFFERS_CONTAINER was not found!');
+}
+
 //-----------------------------------------------------------------------
 // adding offer to DOM
 const dummyData = generateDummyData(1);
